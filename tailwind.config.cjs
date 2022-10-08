bbfd55049f5c6834b1876e7354a3eb4feabd99b4
xiayudev <jctuesta7@gmail.com>
2022-10-07 23:24:41 -0500

    Adding skeleton form
    Signed-off-by: xiayudev <jctuesta7@gmail.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,15 +7,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'Linear gradient (active input border)': 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
-        'Red (input errors)': 'hsl(0, 100%, 66%)',
-        'White': 'hsl(0, 0%, 100%)',
-        'Light grayish violet': 'hsl(270, 3%, 87%)',
-        'Dark grayish violet': 'hsl(279, 6%, 55%)',
-        'Very dark violet': 'hsl(278, 68%, 11%)',
+        'linear-gradient': 'hsl(249, 99%, 64%) to hsl(278, 94%, 30%)',
+        'input-errors)': 'hsl(0, 100%, 66%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'light-grayish-violet': 'hsl(270, 3%, 87%)',
+        'dark-grayish-violet': 'hsl(279, 6%, 55%)',
+        'very-dark-violet': 'hsl(278, 68%, 11%)',
       },
       fontFamily: {
         'grotesk': ['Space Grotesk', 'sans-serif']
+      },
+      screens: {
+        'desktop': '1440px'
       }
     },
   },
